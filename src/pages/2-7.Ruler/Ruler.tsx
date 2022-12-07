591b6cc2e994e5d1d306d80c4336769d03391a1c
김광회 <postelian@gmail.com>
2022-12-07 14:32:53 +0900

    설명 수정

--- src/pages/2-7.Ruler/Ruler.tsx
+++ src/pages/2-7.Ruler/Ruler.tsx
@@ -78,7 +78,7 @@ export const Ruler = () => {
         CSS 적용이 쉽도록 attr() 메서드로 class 속성을 부여한다.<br/>
         이후 attr('transform', 값)으로 상하좌우 여백을 지정한 다음<br/>
         call() 메서드로 눈금자 위치를 지정한다.<br/><br/>
-        아래 데이터는 CSV 파일에 저장된 데이터셋을 가져다 사용한다.<br/>
+        또, 아래 그래프는 CSV 파일에 저장된 10가지 데이터셋을 사용한다.<br/>
         버튼을 눌러 데이터셋을 교체해보자.
       </div>
       <Button color="teal" ripple size="sm" className="mt-4" onClick={() => {
