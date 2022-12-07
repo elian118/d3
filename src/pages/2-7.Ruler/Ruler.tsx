63d2e80f83acd50306469cfb3497551275964f3b
김광회 <postelian@gmail.com>
2022-12-07 14:34:32 +0900

    설명 수정

--- src/pages/2-7.Ruler/Ruler.tsx
+++ src/pages/2-7.Ruler/Ruler.tsx
@@ -74,7 +74,7 @@ export const Ruler = () => {
       <h1>눈금자 막대그래프</h1>
       <div className="text-base my-4 max-w-[500px]">
         눈금자까지 추가하려면 해당 요소를 D3로 추가하면 된다.<br/><br/>
-        일반적으로 눈금자 요소들을 일괄 그루핑하고<br/>
+        일반적으로 눈금자 요소들은 일괄 그루핑하고<br/>
         CSS 적용이 쉽도록 attr() 메서드로 class 속성을 부여한다.<br/>
         이후 attr('transform', 값)으로 상하좌우 여백을 지정한 다음<br/>
         call() 메서드로 눈금자 위치를 지정한다.<br/><br/>
