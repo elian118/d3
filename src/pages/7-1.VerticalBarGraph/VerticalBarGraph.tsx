4c21b637084b12d8e4ba3cbec5f72f2b49949ff2
김광회 <postelian@gmail.com>
2022-12-08 11:01:50 +0900

    세로형 막대그래프 + 값 표시 + 눈금자 적용/위치 조정

--- src/pages/7-1.VerticalBarGraph/VerticalBarGraph.tsx
+++ src/pages/7-1.VerticalBarGraph/VerticalBarGraph.tsx
@@ -1,26 +1,41 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { select } from 'd3';
+import { axisLeft, scaleLinear, select } from 'd3';
 import { dataSet2 } from '@/consts/dataSets';
 import './verticalBarGraphStyle.css';
 
 export const VerticalBarGraph = () => {
   const svgHeight = 240;
+  const offsetX = 30; // x좌표 어긋난 정도
+  const offsetY = 10; // y좌표 어긋난 정도
+
   const svgRef = useRef<SVGSVGElement | null>(null);
   const svg = select(svgRef?.current);
   let barElement;
 
   const [dataSet, setDataSet] = useState<number[]>([]);
 
-  const inputText = () => {
+  // 눈금 표시를 위한 스케일 설정
+  const yScale = scaleLinear().domain([0, 300]).range([300, 0]); // y 좌표가 거꾸로 계산되므로 range 반대로 입력
+
+  const applyRuler = () => {
+    svg.append('g')
+      .attr('class', 'axis')
+      .attr('transform', `translate(${offsetX}, ${svgHeight - 300 - offsetY})`)
+      .call(axisLeft(yScale))
+  }
+
+  // 그래프 안에 텍스트 입력 
+  const inputTextInGraph = () => {
     barElement.enter()
       .append('text')
       .attr('class', 'barNum')
-      .attr('x', (d, i) => i * 25 + 10)
-      .attr('y', svgHeight - 5)
+      .attr('x', (d, i) => i * 25 + 10 + offsetX)
+      .attr('y', svgHeight - 5 - offsetY)
       .text((d) => d); // 데이터 표시
   }
 
-  const init = () => {
+  // 데이터 추가
+  const inputData = () => {
     barElement = svg.selectAll('rect').data(dataSet);
 
     barElement.enter() // 데이터 수만큼 반복
@@ -28,14 +43,15 @@ export const VerticalBarGraph = () => {
       .attr('class', 'bar')
       .attr('height', (d) => d)
       .attr('width', 20)
-      .attr('x', (d, i) => i * 25) // x좌표 지정 => 그래프 간격 설정
-      .attr('y', (d) => svgHeight - d) // y좌표 지정 => 세로 그래프 보정치 부여(막대 그래프 시작점 보정)
+      .attr('x', (d, i) => i * 25 + offsetX) // x좌표 지정 => 그래프 간격 설정 + 보정치 추가
+      .attr('y', (d) => svgHeight - d - offsetY) // y좌표 지정 => 세로 그래프 보정치 부여(막대 그래프 시작점 보정) - 눈금자 중첩을 피하기 위한 보정치 추가
 
-    inputText();
+    inputTextInGraph();
+    applyRuler();
   }
 
   useEffect(() => {
-    dataSet.length > 0 && init();
+    dataSet.length > 0 && inputData();
   }, [dataSet]);
 
   useEffect(() => {
