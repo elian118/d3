75345570bc7545d2c0063f6801bfc91c86c0bb33
김광회 <postelian@gmail.com>
2022-12-25 16:11:42 +0900

    다중 꺾은선 그래프 + 커브 타입 선택(옵션)

--- src/pages/9-3.MultiLineGraph/DrawGraph.tsx
+++ src/pages/9-3.MultiLineGraph/DrawGraph.tsx
@@ -13,7 +13,7 @@ export const DrawGraph: FC<DrawGraphProps> = ({ data, idx, curve }) => {
   let myLines = line<number>()
     .x((d, i) => offsetX + i * margin)
     .y((d) => svgHeight - Number(d) * scale - offsetY)
-    .curve(curve[0]);
+    .curve(curve[0]); // 곡선의 구부러짐 형태를 D3의 CurveFactory 인터페이스 구현체들 중에서 선택할 수 있다.
 
   return <path className={`line item${idx + 1}`} d={myLines(data) ?? '0'} />;
 };
